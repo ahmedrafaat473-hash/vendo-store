a1c5afc75818b8efd30663adf522abd74e7dc073
ahmedrafaat473-hash <ahmedrafaat473@gmail.com>
2026-06-29 22:01:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 function addToCart() {
 
+  let name = "تيشيرت VENDO";
+
+  let size = document.getElementById("size").value;
+  let quality = document.getElementById("quality").value;
+  let qty = parseInt(document.getElementById("qty").innerText);
+
+  let basePrice = 299;
+  let price = basePrice * quality;
+
   let product = {
-    name: "تيشيرت VENDO",
-    price: 299,
-    qty: 1
+    name: name,
+    size: size,
+    quality: quality,
+    price: price,
+    qty: qty
   };
 
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
