7259436bfcccff3221eab745b7c0c3d17ce3b521
ahmedrafaat473-hash <ahmedrafaat473@gmail.com>
2026-06-29 23:40:13 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,33 +1,28 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function renderCart() {
+function renderCart(){
+
   let container = document.getElementById("cartItems");
   container.innerHTML = "";
 
   let total = 0;
 
-  cart.forEach((item, index) => {
+  cart.forEach(item => {
+
     total += item.price * item.qty;
 
     container.innerHTML += `
-      <div class="cart-item">
+      <div>
         <h3>${item.name}</h3>
+        <p>المقاس: ${item.size}</p>
         <p>السعر: ${item.price}</p>
         <p>الكمية: ${item.qty}</p>
-
-        <button onclick="removeItem(${index})">حذف ❌</button>
       </div>
     `;
   });
 
   document.getElementById("cartTotal").innerText =
     "الإجمالي: " + total + " جنيه";
-}
-
-function removeItem(index){
-  cart.splice(index, 1);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  renderCart();
 }
 
 function clearCart(){
