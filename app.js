4a5123d78f02c08867b7df7a28db2f8fb1c668ad
ahmedrafaat473-hash <ahmedrafaat473@gmail.com>
2026-06-29 23:39:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ let qty = 1;
 let basePrice = 299;
 let selectedPrice = basePrice;
 
-function changeQty(value) {
-  qty += value;
-  if (qty < 1) qty = 1;
+function changeQty(v){
+  qty += v;
+  if(qty < 1) qty = 1;
   document.getElementById("qty").innerText = qty;
   updateTotal();
 }
 
-function updatePrice() {
-  let quality = document.getElementById("quality").value;
-  selectedPrice = basePrice * quality;
+function updatePrice(){
+  let q = document.getElementById("quality").value;
+  selectedPrice = basePrice * q;
   updateTotal();
 }
 
-function updateTotal() {
+function updateTotal(){
   let total = selectedPrice * qty;
   document.getElementById("totalPrice").innerText =
     "الإجمالي: " + total + " جنيه";
 }
 
-function addToCart() {
+function addToCart(){
 
   let product = {
     name: "تيشيرت VENDO",
